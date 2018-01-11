c7abd490b0402d81ff24069369084d2f9a78f2dd
Fred-el-Jolo <guillaume.frederic@gmail.com>
2018-01-11 09:53:38 +0100

    Publish as a npm package

--- index.js
+++ index.js
@@ -1,32 +1,3 @@
 const EventBus = require('./src/event-bus');
 
-const eventBus = new EventBus();
-
-//----------------------------------
-eventBus.on('one', function() {
-    console.log('one');
-});
-eventBus.emit('one');
-
-//-----------------------------------
-eventBus.on('two', function() {
-    console.log('two');
-});
-eventBus.emit('one');
-eventBus.emit('two');
-eventBus.emit('three');
-
-//-----------------------------------
-eventBus.on('echo', function(str) {
-    console.log('echo:', str);
-});
-eventBus.emit('echo', 'hello world');
-
-//-----------------------------------
-eventBus.on('log', function() {
-    console.log('log:', arguments);
-});
-eventBus.emit('log', 'hello', 'world', '!');
-
-//-----------------------------------
-eventBus.emit('one', 'two', 'log','hello', 'world', '!');
+module.exports = EventBus;
